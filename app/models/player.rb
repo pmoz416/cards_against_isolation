e04bb6f212ead9ca00970ea96df80208d1e87337
pmoz416 <pmorren@pm.me>
2020-09-06 13:51:57 +1000

    Create Player as a Devise/authenticable model

---
+++ app/models/player.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class Player < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :confirmable, :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
