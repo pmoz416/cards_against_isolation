c70ee35053614ecd968858afdfd72e7035040d92
pmoz416 <pmorren@pm.me>
2020-09-20 08:20:57 +1000

    Install Tailwind CSS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,6 +5,8 @@
 
 require("@rails/ujs").start()
 require("channels")
+import "stylesheets/application"
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
